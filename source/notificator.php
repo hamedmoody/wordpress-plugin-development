2835bb138765a69b2ec23363aba6af825eaca1e4
Hamed Moodi <hamedmoodi2011@gmail.com>
2023-01-06 12:11:13 +0330

    [ADD] Add icon and notificator function

---
+++ source/notificator.php
@@ -0,0 +1,23 @@
+<?php
+function notificator_send_message( $text ){
+
+    $postArgs           = array();
+    $postArgs['to']     = 'Your_token_here';
+    $postArgs['text']   = $text;
+
+    $ch = curl_init( 'https://notificator.ir/api/v1/send' );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postArgs );
+
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5 );
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5 );
+
+    // execute!
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    return json_decode( $response );
+    
+}
