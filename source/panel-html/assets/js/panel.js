f1762668e48869c49a6dcc5f650386d263ab2447
Hamed Moodi <hamedmoodi2011@gmail.com>
2023-06-24 09:08:04 +0330

    Complete Dashboard

--- source/panel-html/assets/js/panel.js
+++ source/panel-html/assets/js/panel.js
@@ -1,3 +1,13 @@
 jQuery(document).ready( function( $ ){
-    
+
+    /**
+     * https://github.com/CodeSeven/toastr
+     */
+    toastr.options.positionClass    = 'toast-top-center';
+    toastr.options.progressBar      = true;
+    toastr.options.rtl              = true;
+
+    toastr.success('با موفقیت وارد سایت شدید', 'ورود موفق')
+
+
 } );
